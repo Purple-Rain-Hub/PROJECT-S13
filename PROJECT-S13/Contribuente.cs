7056b65baec4b97e1d99f3b90893bf32a257e3fe
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-02-14 10:32:29 +0100

    Aggiungere i file di progetto.

---
+++ PROJECT-S13/Contribuente.cs
@@ -0,0 +1,24 @@
+﻿
+namespace PROJECT_S13
+{
+    internal class Contribuente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public string CodiceFiscale { get; set; }
+        public double RedditoAnnuale { get; set; }
+
+        private void TaxCalculation()
+        {
+            Console.WriteLine("ciao");
+        }
+
+        private void TaxShow()
+        {
+            Console.WriteLine("ciao");
+        }
+    }
+}
